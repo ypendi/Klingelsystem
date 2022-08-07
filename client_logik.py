2647665b37a48bd5359d9806a116676deab24179
pandy <lukaspendi1543@gmail.com>
2022-08-07 13:45:53 +0200

    basically done

---
+++ client_logik.py
@@ -0,0 +1,82 @@
+import socket 
+
+from PyQt5.QtCore import pyqtSignal, QThread
+
+class Client_Receiver(QThread):
+
+    SERVER_IP = "192.168.56.1" # set SERVER to servers IP 
+    PORT = 5050
+    client_socket = None
+    connected = False
+
+    FORMAT = "utf-8"
+    DISCONNECT_MSG = "/disconnect"
+    KLINGEL_MSG = "KLINGEL"
+
+    gotMsg = pyqtSignal([str])
+
+    def __init__(self, ip=SERVER_IP, port=PORT):
+        super(QThread, self).__init__()
+        self.SERVER_IP = ip
+        self.PORT = port
+        self.verbinde_client()
+
+    def startThread(self):
+        if self.connected == False:
+            print("Nicht mit Server verbunden")
+            return
+        if self.isRunning():
+            print("läuft bereits!")
+            return
+        self.start()
+    
+    def stopThread(self):
+        if not self.isRunning():
+            print("not running!")
+        else:
+            self.requestInterruption()
+            self.quit()
+            if not self.wait(100):
+                self.terminate()
+                if not self.wait(100):
+                    print("Terminate failed!") # asdklfjhaslödjfnoklasdf fragen
+        self.disconnect()
+
+    def verbinde_client(self):
+        if self.connected == False:
+            try:
+                self.connected = True 
+                self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                self.client_socket.connect((self.SERVER_IP, self.PORT))
+                print(f"Verbunden:{self.SERVER_IP}:{self.PORT}")
+            except Exception as e:
+                print("Verbinden fehlgeschlagen", e)
+                self.connected = False
+                self.client_socket = None
+
+    def disconnect(self):
+        self.client_socket.send(self.DISCONNECT_MSG.encode(self.FORMAT))
+        self.client_socket.close()
+        self.connected = False 
+
+    def message_server(self, message):
+        message = message.encode(self.FORMAT)
+        self.client_socket.send(message)
+
+    def run(self): # run
+        try: 
+            while not self.isInterruptionRequested():
+                msg = self.client_socket.recv(1024).decode(self.FORMAT)
+
+                if msg == self.KLINGEL_MSG:
+                    print(msg)
+                    self.gotMsg.emit(msg)
+                elif msg == "JEMAND_GEHT":
+                    print(msg)
+                    self.gotMsg.emit(msg)
+                else:
+                    print("MSG:", msg)    
+
+        except Exception as f:
+            print("Error: ", f)
+            return
---
+++ server_logik.py
@@ -0,0 +1,84 @@
+import socket 
+import threading
+
+PORT = 5050
+SERVER = socket.gethostbyname(socket.gethostname()) # set SERVER to current devices IP address
+ADDR = (SERVER, PORT)
+
+HEADER = 64 # every first msg from client to server has to be 64bytes (first msg is amount of bytes of the actual msg)
+FORMAT = 'utf-8'
+DISCONNECT_MSG = "/disconnect"
+
+clientlist = []
+
+
+def starte_server():
+    try:
+        global server
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # create a new IPv4 STREAM socket
+        server.bind(ADDR)
+        main()
+    except:
+        print("Error: Server läuft bereits")
+
+
+def handle_client(clientsocket, addr):
+    print(f"[NEW CONNECTION] addr: {addr}")
+
+    connected = True
+
+    while connected:
+        try:
+            message = clientsocket.recv(1024).decode(FORMAT)
+            print(f"[{addr}]: {message}")
+
+
+            if message == "ICH_GEHE_NICHT":
+                pass
+            elif message == "ICH_GEHE": # wenn einer der clients "zur klingel geht"
+                for client in clientlist:   # sende signal an alle anderen clients 
+                    print(client.getpeername())
+                    if(client != clientsocket):
+                        message = "JEMAND_GEHT".encode(FORMAT)
+                        client.send(message)
+
+            elif message == DISCONNECT_MSG: # break while loop
+                connected = False
+                clientlist.remove(clientsocket)
+                break
+        except:
+            connected = False
+            break
+
+
+    
+    print(f"[DISCONNECT] {addr} disconnected.")
+    print(f"[ACTIVE CONNECTIONS]: {threading.activeCount() -2}")
+    clientsocket.close() # close connection
+
+def sende_klingelsignal_an_clients():
+    message = "KLINGEL"
+    message = message.encode(FORMAT)
+    
+    if not clientlist: # list is empty 
+        print("Error: keine Clients verbunden")
+    else:
+        for client in clientlist:
+            # client.send(send_length)
+            client.send(message)
+
+
+def main():
+    # starte_server()
+    server.listen()
+    print(f"[STARTING] server is running on {SERVER}")
+    while True:
+        clientsocket, addr = server.accept() # wait for new connection, store ip in addr and store socket obj in conn
+        clientlist.append(clientsocket)
+        thread = threading.Thread(target=handle_client, args=(clientsocket, addr)) # create a new thread of the handle_client function
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS]: {threading.activeCount() -1}")
+
+# main()
+
+
